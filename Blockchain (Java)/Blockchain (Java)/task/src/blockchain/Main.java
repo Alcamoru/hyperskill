bdda4e0daf288d5a46bc0f112d64508a2cc7162c
Alexis <alcamoru@icloud.com>
2023-08-12 11:14:07 +0200

    Add blockchain project

---
+++ Blockchain (Java)/Blockchain (Java)/task/src/blockchain/Main.java
@@ -0,0 +1,24 @@
+package blockchain;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        long timeStamp;
+        String previousHash = "0";
+        for (int i = 1; i <= 5; i++) {
+            timeStamp = new Date().getTime();
+            System.out.println("Block:");
+            System.out.println("Id: " + i);
+            System.out.println("Timestamp: " + timeStamp);
+            System.out.println("Hash of the previous block: ");
+            System.out.println(previousHash);
+            System.out.println("Hash of the block: ");
+            Block block = new Block(i, timeStamp, previousHash);
+            String hashBlock = block.calculateHash();
+            System.out.println(hashBlock + "\n");
+            previousHash = hashBlock;
+        }
+
+    }
+}
