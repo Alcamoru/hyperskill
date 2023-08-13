be5a4e8a21a35d5cd529198a88a3de3c7d876999
Alexis <alcamoru@icloud.com>
2023-08-13 09:16:35 +0200

    Completed stage 1 (blockchain)

--- Blockchain (Java)/Blockchain (Java)/task/src/blockchain/Main.java
+++ Blockchain (Java)/Blockchain (Java)/task/src/blockchain/Main.java
@@ -1,24 +1,14 @@
 package blockchain;
 
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
-        long timeStamp;
-        String previousHash = "0";
-        for (int i = 1; i <= 5; i++) {
-            timeStamp = new Date().getTime();
-            System.out.println("Block:");
-            System.out.println("Id: " + i);
-            System.out.println("Timestamp: " + timeStamp);
-            System.out.println("Hash of the previous block: ");
-            System.out.println(previousHash);
-            System.out.println("Hash of the block: ");
-            Block block = new Block(i, timeStamp, previousHash);
-            String hashBlock = block.calculateHash();
-            System.out.println(hashBlock + "\n");
-            previousHash = hashBlock;
-        }
+        Blockchain blockchain = new Blockchain();
+        blockchain.createBlock();
+        blockchain.createBlock();
+        blockchain.createBlock();
+        blockchain.createBlock();
+        blockchain.createBlock();
 
+        System.out.println(blockchain);
     }
 }
